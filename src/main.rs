b94eaeacf097fc04a397b9f0bb6c91db84077413
James Bates <jmbates@gmail.com>
2021-11-28 12:43:59 -0600

    major update: eliminated inefficiencies, improved speed by 15%

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 #![feature(core_intrinsics)]
+#![allow(dead_code)]
 
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 use faf::const_concat_bytes;
-use faf::const_config::*;
 use faf::const_http::*;
-use faf::extern_http_date;
 use faf::util::{const_len, memcmp};
 use std::intrinsics::likely;
 
@@ -35,29 +34,28 @@ const PLAINTEXT_BASE: &[u8] = const_concat_bytes!(
 
 const PLAINTEXT_BASE_LEN: usize = const_len(PLAINTEXT_BASE);
 
+const PLAINTEXT_TEST: &[u8] = b"HTTP/1.1 200 OK\r\nServer: F\r\nContent-Type: text/plain\r\nContent-Length: 13\r\nDate: Thu, 18 Nov 2021 23:15:07 GMT\r\n\r\nHello, World!";
+const PLAINTEXT_TEST_LEN: usize = const_len(PLAINTEXT_TEST);
+
 #[inline]
 fn cb(
-   method: *const i8,
+   method: *const u8,
    method_len: usize,
-   path: *const i8,
+   path: *const u8,
    path_len: usize,
-   _headers: &[faf::phr_header; MAX_HEADERS_TO_PARSE],
-   _num_headers: usize,
    response_buffer: *mut u8,
+   date_buff: *const u8,
 ) -> usize {
    unsafe {
+      // std::ptr::copy_nonoverlapping(PLAINTEXT_TEST.as_ptr(), response_buffer, PLAINTEXT_TEST_LEN);
+      // return PLAINTEXT_TEST_LEN;
+
       if likely(method_len >= GET_LEN && path_len >= ROUTE_PLAINTEXT_LEN) {
-         if likely(memcmp(GET.as_ptr() as *const i8, method, GET_LEN) == 0) {
+         if likely(memcmp(GET.as_ptr(), method, GET_LEN) == 0) {
             // For performance purposes, this will successfully match '/p' to '/plaintext' and '/pickle'. Use with caution
-            if likely(memcmp(ROUTE_PLAINTEXT.as_ptr() as *const i8, path, ROUTE_PLAINTEXT_LEN) == 0) {
-               let mut date_buff = crate::extern_http_date::get_buff_with_date();
-               extern_http_date::get_http_date(&mut date_buff);
+            if likely(memcmp(ROUTE_PLAINTEXT.as_ptr(), path, ROUTE_PLAINTEXT_LEN) == 0) {
                std::ptr::copy_nonoverlapping(PLAINTEXT_BASE.as_ptr(), response_buffer, PLAINTEXT_BASE_LEN);
-               std::ptr::copy_nonoverlapping(
-                  date_buff.as_ptr(),
-                  response_buffer.add(PLAINTEXT_BASE_LEN),
-                  DATE_LEN,
-               );
+               std::ptr::copy_nonoverlapping(date_buff, response_buffer.add(PLAINTEXT_BASE_LEN), DATE_LEN);
                std::ptr::copy_nonoverlapping(
                   CRLFCRLF.as_ptr(),
                   response_buffer.add(PLAINTEXT_BASE_LEN + DATE_LEN),
@@ -71,19 +69,11 @@ fn cb(
 
                PLAINTEXT_BASE_LEN + DATE_LEN + CRLFCRLF_LEN + PLAINTEXT_BODY_LEN
             } else {
-               std::ptr::copy_nonoverlapping(
-                  HTTP_404_NOTFOUND.as_ptr(),
-                  response_buffer,
-                  HTTP_404_NOTFOUND_LEN,
-               );
+               std::ptr::copy_nonoverlapping(HTTP_404_NOTFOUND.as_ptr(), response_buffer, HTTP_404_NOTFOUND_LEN);
                HTTP_404_NOTFOUND_LEN
             }
          } else {
-            std::ptr::copy_nonoverlapping(
-               HTTP_405_NOTALLOWED.as_ptr(),
-               response_buffer,
-               HTTP_405_NOTALLOWED_LEN,
-            );
+            std::ptr::copy_nonoverlapping(HTTP_405_NOTALLOWED.as_ptr(), response_buffer, HTTP_405_NOTALLOWED_LEN);
             HTTP_405_NOTALLOWED_LEN
          }
       } else {
