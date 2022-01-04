a1ad3586f190895a2af54a4c59e3c38c2492c122
James Bates <errantmind@protonmail.com>
2022-01-04 17:22:38 -0600

    add script to compile with no flags

---
+++ compile_no_flags.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+ cargo build --release --target x86_64-unknown-linux-gnu -Zbuild-std=panic_abort,core,std,alloc,proc_macro,compiler_builtins \
+ && strip ./target/x86_64-unknown-linux-gnu/release/faf-ex
